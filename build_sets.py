c1c34c394eb5c5f316e8e0c5fbf80f35c197ca96
Louis Sugy <louis.sugy@gmail.com>
2019-11-12 02:48:19 +0100

    Building only training and inference set, the framework does the split

--- build_sets.py
+++ build_sets.py
@@ -2,23 +2,8 @@ import argparse
 import csv
 import random
 
-from config import tags_to_features, feature_price, feature_achievements, \
-    feature_controller
-
-
-def get_args():
-    parser = argparse.ArgumentParser(
-        description='Generates the training and test sets.')
-    parser.add_argument("test_ratio", type=float,
-                        help="Ratio of samples to use as the test set"
-                             " (between 0 and 1)")
-
-    return parser.parse_args()
-
 
 def main():
-    args = get_args()
-
     with open('game_details.csv', 'r') as csv_file:
         reader = csv.reader(csv_file)
         game_details = list(reader)
@@ -29,26 +14,17 @@ def main():
         reader = csv.reader(csv_file)
         game_notes = list(reader)
 
-    total_set = [[note[0]] + details_dic[note[0]] + [note[1]]
-                 for note in game_notes if note[0] in details_dic]
-    all_ids = set(ts[0] for ts in total_set)
-    test_ids = set(random.sample(all_ids, int(len(all_ids) * args.test_ratio)))
+    all_ids = set(note[0] for note in game_notes
+                  if note[0] in details_dic)
+    training_set = [details_dic[note[0]] + [note[1]]
+                    for note in game_notes if note[0] in all_ids]
 
-    test_set = [ts[1:] for ts in total_set if ts[0] in test_ids]
-    training_set = [ts[1:] for ts in total_set
-                    if ts[0] not in test_ids]
-    random.shuffle(test_set)
     random.shuffle(training_set)
 
     with open("training_set.csv", "w") as csv_file:
         writer = csv.writer(csv_file, lineterminator="\n")
         writer.writerows(training_set)
     print("training_set.csv written")
-
-    with open("test_set.csv", "w") as csv_file:
-        writer = csv.writer(csv_file, lineterminator="\n")
-        writer.writerows(test_set)
-    print("test_set.csv written")
 
     inference_set = [details for key, details in details_dic.items()
                      if key not in all_ids]
